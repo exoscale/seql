5de3d0b0e5687a96c50f54e300bba670c1689b18
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-09-05 09:54:15 +0200

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/seql "0.1.2"
+(defproject exoscale/seql "0.1.3-SNAPSHOT"
   :description "Simplfied EDN Query Language for SQL"
   :url "https://github.com/exoscale/bundestore"
   :license {:name "ISC"}
