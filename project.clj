239a7a82cb22d76f8cbfe2f4a3b8fcf6bdb88ff8
Max Penet <max.penet@exoscale.ch>
2019-11-07 13:46:33 +0100

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
               [k version :exclusions exclusions])
       paths (:paths cfg)]
 
-  (defproject exoscale/seql "0.1.6"
+  (defproject exoscale/seql "0.1.7-SNAPSHOT"
     :description "Simplfied EDN Query Language for SQL"
     :url "https://github.com/exoscale/seql"
     :license {:name "MIT/ISC License"}
