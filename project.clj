5b981ff92769a9547b99e8be1fca67ee2a832bc6
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-10-15 13:40:38 +0200

    Version 0.1.30-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/seql "0.1.29"
+(defproject exoscale/seql "0.1.30-SNAPSHOT"
   :description "Simplfied EDN Query Language for SQL"
   :url "https://github.com/exoscale/seql"
   :license {:name "MIT/ISC License"}
